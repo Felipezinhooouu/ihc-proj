04201cf1c3f29061f0a8b1194e124dad5e94161b
Felipe Mariano <felipemarianodasilva@gmail.com>
2016-10-27 23:10:47 -0200

    Removes aditional code and adds seeder again

--- app.js
+++ app.js
@@ -83,8 +83,9 @@ mongoose.Promise = global.Promise;
 
 var url = 'mongodb://' + process.env.MONGODB_SERVICE_HOST;
 
-if (process.env.OPENSHIFT_MONGO_DB_URL){
-  url = process.env.OPENSHIFT_MONGODB_DB_URL +
+//Openshift options!
+if (process.env.OPENSHIFT_MONGODB_DB_URL){
+  url = process.env.OPENSHIFT_MONGODB_DB_URL + '/' +
   process.env.OPENSHIFT_APP_NAME;
 }
 
@@ -94,7 +95,7 @@ var options = {
   user: process.env[mongoServiceName + '_USER'],
   pass: process.env[mongoServiceName + '_PASSWORD']
 }
-
+//Check control access
 console.log("CONNECTING TO: " + url);
 console.log("WITH USER: " + options.user);
 console.log("WITH PASS: " + options.pass);
@@ -102,66 +103,19 @@ console.log("WITH PASS: " + options.pass);
 var connect = function() {
   mongoose.connect(url, options).then(() => {
     console.log("CONNECTED SUCCESSFULLY");
+
+    console.log("----- Applying seeds:\n");
+    seeder();
+    console.log("------------");
   }).catch((err) => console.error(err));
 }
 
 connect();
 
-/////
-
-
-// Connect to mongodb
  var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
    ip = process.env.IP || process.env.OPENSHFT_NODEJS_IP || '0.0.0.0',
    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
    mongoURLLabel = "";
-//
-// if (mongoURL == null && process.env.DATABASE_SERVICE_NAME){
-//   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-//   mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-//   mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-//   mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-//   mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-//   mongoUser = process.env[mongoServiceName + '_USER'];
-//
-//   if(mongoHost && mongoPort && mongoDatabase){
-//     mongoURLLabel = mongoURL = 'mongodb://';
-//     if(mongoUser && mongoPassword){
-//       mongoURL += mongoUser + ':' + mongoPassword + '@';
-//     }
-//
-//     mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-//     mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-//   }
-// }
-//
-// var db = null, dbDetails = new Object();
-
-//var initDb = function(callback){
-  //if (mongoURL == null) return;
-
-  // var mongodb = require('mongodb');
-  // if (mongodb == null) return;
-  //
-  // mongodb.connect(mongoURL, function(err, conn){
-  //   if(err){
-  //     callback(err);
-  //     return;
-  //   }
-
-
-    // db = conn;
-    // dbDetails.databaseName = db.databaseName;
-    // dbDetails.url = mongoURLLabel;
-    // dbDetails.type = 'MongoDB';
-
-  //  console.log('Connected to MongoDB at: %s', mongoURL);
-  //});
-//}
-
-// initDb(function(err){
-//   console.log('erro ao conectar no mongoDB: ' + err);
-// });
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
