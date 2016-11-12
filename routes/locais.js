7f1d47c68325db02e4ebf37fcfee2dce4ae1d7f3
Felipe Mariano <felipemarianodasilva@gmail.com>
2016-11-12 15:28:14 -0200

    Tests get doencas of locais

--- routes/locais.js
+++ routes/locais.js
@@ -3,12 +3,20 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var Local = require('../models/Local');
+var Vacina = require('../models/Vacina');
 
 router.get('/', function(req, res, next){
   Local.find(function(err, all){
     if(err) return next(err);
     res.json(all);
   })
+});
+
+router.get('/:doenca', function(req, res, next){
+  Vacina.find({"nome": {"$regex": req.params.doenca, "$options": "i"}}
+      , function(err, post){
+        res.json(post);
+      });
 });
 
 module.exports = router;
